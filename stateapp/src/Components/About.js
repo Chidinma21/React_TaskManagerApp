34ff07125214fe86739854f66421f71b1676e6ef
Chidinma21 <chidinmaonuora1@gmail.com>
2022-04-10 22:20:12 +0100

    first commit

---
+++ stateapp/src/Components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+const About = () => {
+  return (<div>
+      <h4>Version 5.6.3</h4>
+      <Link to="/">Go Back</Link>
+  </div>);
+};
+
+export default About;
